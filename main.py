c26eb04dbb73fbe4cba298ab909cd09017838c5b
alsvike <als002@edu.zealand.dk>
2025-09-30 09:44:36 +0200

    Add imports and dependency assertions to main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-# Imports/Dependencies
+import sys
+assert sys.version_info >= (3, 7)
+from packaging import version
+import sklearn
+assert version.parse(sklearn.__version__) >= version.parse("1.0.1")
+import pandas as pd
+from pathlib import Path
+import numpy as np
+
